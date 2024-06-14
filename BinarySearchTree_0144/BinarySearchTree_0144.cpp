4c094e33443e6b71aef4fa6b96f95046863331a7
M S I <M S I@MSI>
2024-06-14 10:41:34 +0700

    insert node ke binary

--- BinarySearchTree_0144/BinarySearchTree_0144.cpp
+++ BinarySearchTree_0144/BinarySearchTree_0144.cpp
@@ -24,4 +24,13 @@ public:
 		ROOT = nullptr;  // Initializing ROOT to null
 	}
 
-	
+	void insert(string element) { // Insert a Node in a Binary Search Tree
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memmory for the new node
+		newNode->info = element;  // Assign to the data field of the new node
+		newNode->leftchild = nullptr;  //Make the left child of the new node point to null
+		newNode->rightchild = nullptr; // Make the right child of the new node point to null
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+
+		
