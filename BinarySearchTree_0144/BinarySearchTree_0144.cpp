27741b283895d1f6a78d7b514e3fcbe17757f7a8
M S I <M S I@MSI>
2024-06-14 10:40:02 +0700

    membuat class node

--- BinarySearchTree_0144/BinarySearchTree_0144.cpp
+++ BinarySearchTree_0144/BinarySearchTree_0144.cpp
@@ -2,3 +2,16 @@
 #include <string>
 using namespace std;
 
+class Node {
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	Node(string i, Node* l, Node* r) {
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+
