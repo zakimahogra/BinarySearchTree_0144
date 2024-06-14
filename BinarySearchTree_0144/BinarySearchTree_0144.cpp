0cf86a0b0b2038389d7cb8ff36c0139df0aa160b
M S I <M S I@MSI>
2024-06-14 10:51:03 +0700

    switch case

--- BinarySearchTree_0144/BinarySearchTree_0144.cpp
+++ BinarySearchTree_0144/BinarySearchTree_0144.cpp
@@ -118,4 +118,42 @@ int main()
 		cout << "5. Exit" << endl;
 		cout << "\nEnter your choice (1-5) : ";
 
-		
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
+		case '2':
+		{
+			x.inorder(x.ROOT);
+			break;
+		}
+		case '3':
+		{
+			x.preorder(x.ROOT);
+			break;
+		}
+		case '4':
+		{
+			x.postorder(x.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
+}
